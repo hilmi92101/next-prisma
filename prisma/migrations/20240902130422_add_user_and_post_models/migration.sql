4bc5aa81b8979cd66096067d9376c095c99894e1
hilmi <muhammadhilmi92101@gmail.com>
2024-09-02 21:12:09 +0800

    update script command at package.json

--- prisma/migrations/20240902130422_add_user_and_post_models/migration.sql
+++ prisma/migrations/20240902130422_add_user_and_post_models/migration.sql
@@ -1,11 +1,13 @@
 -- CreateTable
 CREATE TABLE `users` (
     `id` VARCHAR(191) NOT NULL,
+    `username` VARCHAR(191) NULL,
     `email` VARCHAR(191) NOT NULL,
     `hashedPassword` VARCHAR(191) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `users_username_key`(`username`),
     UNIQUE INDEX `users_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -15,6 +17,7 @@ CREATE TABLE `posts` (
     `id` VARCHAR(191) NOT NULL,
     `title` VARCHAR(191) NOT NULL,
     `slug` VARCHAR(191) NOT NULL,
+    `summary` TINYTEXT NULL,
     `content` TEXT NOT NULL,
     `published` BOOLEAN NULL DEFAULT false,
     `authorId` VARCHAR(191) NULL,
